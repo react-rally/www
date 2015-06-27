37f18898f8540e2b25dbea1b57e857572e0aef9f
mzabriskie <mzabriskie@gmail.com>
2015-06-27 14:06:35 -0600

    Preparing schedule section

--- app/screens/Schedule/components/Session.js
+++ app/screens/Schedule/components/Session.js
@@ -3,19 +3,30 @@ import StyleSheet from 'react-style';
 import { Styles } from 'helpers/constants';
 import borderRadius from 'helpers/borderRadius';
 
-// rgb(48, 96, 125)
-
 const STYLES = StyleSheet.create({
   container: {
+    position: 'relative',
+    margin: '0 0 100px',
+    zoom: 1,
+    overflow: 'auto'
+  },
+  content: {
     background: 'rgb(27, 54, 70)',
     padding: '20px 50px',
-    width: 300
+    width: 300,
+    position: 'relative'
   },
   topic: {
     color: Styles.FONT_COLOR_SCHEDULE__TOPIC
   },
   description: {
     color: Styles.FONT_COLOR_SCHEDULE__DESCRIPTION
+  },
+  pole: {
+    height: 4,
+    backgroundColor: 'rgb(50, 96, 124)',
+    position: 'absolute',
+    top: '50%'
   }
 });
 
@@ -29,25 +40,39 @@ function randomRadius(factor) {
 
 export default class Session extends Component { 
   render() {
-    var containerStyles = STYLES.container;
+    var contentStyles = STYLES.content;
+    var poleStyles = STYLES.pole;
+
     borderRadius(
-      containerStyles,
+      contentStyles,
       randomRadius(),
       randomRadius(),
       randomRadius(),
       randomRadius()
     );
 
+    contentStyles.left = this.props.orient === 'left' ? 100 : null;
+    contentStyles.right = this.props.orient === 'right' ? 100 : null;
+    poleStyles.left = this.props.orient === 'left' ? 150 : 0;
+    poleStyles.right = this.props.orient === 'right' ? 150 : 0;
+    if (this.props.orient === 'right') {
+      contentStyles.float = 'right';
+    }
+
     return (
-      <div style={containerStyles}>
-        <div style={STYLES.topic}>{this.props.topic}</div>
-        <div style={STYLES.description}>{this.props.description}</div>
+      <div style={STYLES.container}>
+        <div style={poleStyles}></div>
+        <div style={contentStyles}>
+          <div style={STYLES.topic}>{this.props.topic}</div>
+          <div style={STYLES.description}>{this.props.description}</div>
+        </div>
       </div>
     );
   }
 }
 
 Session.propTypes = {
+  orient: PropTypes.oneOf(['left', 'right']),
   topic: PropTypes.string,
   description: PropTypes.string
 };
--- app/screens/Schedule/index.js
+++ app/screens/Schedule/index.js
@@ -3,14 +3,17 @@ import StyleSheet from 'react-style';
 import { Styles, Links } from 'helpers/constants';
 import Session from './components/Session';
 
+const poleColor = 'rgb(50, 96, 124)';
 const STYLES = StyleSheet.create({
   section: {
     background: `url(assets/img/weatheredBackground.png) ${Styles.BG_COLOR_SCHEDULE}`,
-    minHeight: 350
-    // minHeight: 1000
+    paddingBottom: 150
   },
   container: {
-    position: 'relative'
+    position: 'relative',
+    padding: '0 100px',
+    minHeight: 1000
+    // minHeight: 2500
   },
   h2: {
     color: Styles.FONT_COLOR_SCHEDULE
@@ -21,6 +24,68 @@ const STYLES = StyleSheet.create({
   },
   a: {
     color: Styles.FONT_COLOR_SCHEDULE
+  },
+  pole: {
+    width: 4,
+    marginLeft: -2,
+    backgroundColor: poleColor,
+    position: 'absolute',
+    top: -15,
+    bottom: -15,
+    left: '50%'
+  },
+  poleTopOuter: {
+    width: 75,
+    height: 75,
+    borderRadius: 75,
+    marginLeft: -37,
+    border: '7px solid ' + poleColor,
+    padding: 7,
+    position: 'absolute',
+    top: -75,
+    left: '50%'
+  },
+  poleTopInner: {
+    width: 47,
+    height: 47,
+    borderRadius: 47,
+    backgroundColor: poleColor
+  },
+  poleBottomOuter: {
+    width: 75,
+    height: 75,
+    borderRadius: 75,
+    marginLeft: -37,
+    border: '2px solid ' + poleColor,
+    padding: 10,
+    position: 'absolute',
+    bottom: -75,
+    left: '50%'
+  },
+  poleBottomInner: {
+    width: 51,
+    height: 51,
+    borderRadius: 51,
+    border: '6px solid ' + poleColor
+  },
+  orientLeft: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: '50%'
+  },
+  orientRight: {
+    position: 'absolute',
+    top: 125,
+    right: 0,
+    bottom: 0,
+    left: '50%'
+  },
+  dayHeader: {
+    marginLeft: 100,
+    marginBottom: 50,
+    color: poleColor
   }
 });
 
@@ -29,16 +94,50 @@ export default class Schedule extends Component {
     return (
       <section id="schedule" style={STYLES.section}>
         <h2 style={STYLES.h2}>Schedule</h2>
-        <p>
-          Our CFP has ended, and we are busy sorting through all your amazing proposals.
-          We will be announcing the schedule once the speakers have been selected.
-        </p>
-        {/*
         <img src="assets/img/cropCircle.png" style={STYLES.img}/>
         <div style={STYLES.container}>
-          <Session topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+          <div style={STYLES.pole}></div>
+          <div style={STYLES.poleTopOuter}>
+            <div style={STYLES.poleTopInner}></div>
+          </div>
+          <div style={STYLES.poleBottomOuter}>
+            <div style={STYLES.poleBottomInner}></div>
+          </div>
+          <div style={STYLES.orientLeft}>
+            <h3 style={STYLES.dayHeader}>Monday, August 24th, 2015</h3>
+            <p>
+              Our CFP has ended, and we are busy sorting through all your amazing proposals.
+              We will be announcing the schedule once the speakers have been selected.
+            </p>
+            {/*
+            <Session orient="left" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="left" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="left" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="left" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="left" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="left" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="left" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="left" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="left" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="left" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            */}
+          </div>
+          <div style={STYLES.orientRight}>
+            <h3 style={STYLES.dayHeader}>Tuesday, August 25th, 2015</h3>
+            {/*
+            <Session orient="right" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="right" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="right" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="right" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="right" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="right" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="right" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="right" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="right" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            <Session orient="right" topic="Ancient alien" description="extraterrestrial origin legendary times Machu Picchu"/>
+            */}
+          </div>
         </div>
-        */}
       </section>
     );
   }
