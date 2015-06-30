996a4a8e70952c87d9b0764cd1b438f8b4bc4af2
mzabriskie <mzabriskie@gmail.com>
2015-06-30 13:15:22 -0600

    Better styles for prospectus button

--- app/screens/Sponsors/index.js
+++ app/screens/Sponsors/index.js
@@ -11,19 +11,27 @@ const STYLES = StyleSheet.create({
   h2: {
     color: Styles.FONT_COLOR_SPONSORS
   },
+  h3: {
+    textAlign: 'center',
+    fontStyle: 'italic'
+  },
   content: {
     textAlign: 'center'
   },
   img: {
-    width: 150,
-    height: 150,
-    background: '#999',
-    borderRadius: 75,
     display: 'inline-block',
-    margin: 100
+    margin: 25
   },
   p: {
     textAlign: 'center'
+  },
+  buttonLink: {
+    color: '#000',
+    borderColor: '#000',
+    ':hover': {
+      backgroundColor: '#000',
+      color: '#fff'
+    }
   }
 });
 
@@ -32,18 +40,14 @@ export default class Sponsors extends Component {
     return (
       <section id="sponsors" style={STYLES.section}>
         <h2 style={STYLES.h2}>Sponsors</h2>
-        {/*
-        <div style={STYLES.content}>
-          <div style={STYLES.img}></div>
-          <div style={STYLES.img}></div>
-          <div style={STYLES.img}></div>
-          <div style={STYLES.img}></div>
-          <div style={STYLES.img}></div>
-        </div>
-        */}
         <p style={STYLES.p}>
           Want to help support React Rally? We'd love to talk with you.<br/>
-          <ButtonLink href={Links.SPONSOR_PROSPECTUS}>Download our prospectus</ButtonLink>
+          <ButtonLink
+            href={Links.SPONSOR_PROSPECTUS}
+            style={STYLES.buttonLink}
+          >
+            Download our prospectus
+          </ButtonLink>
         </p>
       </section>
     );
