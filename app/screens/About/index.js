45afed2a24a5dbb74b54a202bc3e27a5b04a82f3
mzabriskie <mzabriskie@gmail.com>
2015-08-14 14:04:22 -0500

    Adding workshop to About

--- app/screens/About/index.js
+++ app/screens/About/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import StyleSheet from 'react-style';
-import { Styles } from 'helpers/constants';
+import { Styles, Links } from 'helpers/constants';
 
 const STYLES = StyleSheet.create({
   section: {
@@ -34,6 +34,10 @@ export default class About extends Component {
     var fluxLink = this.createLink('https://facebook.github.io/flux/', 'Flux');
     var es6Link = this.createLink('http://es6-features.org/', 'ES6');
     var isoJSLink = this.createLink('https://reactjsnews.com/isomorphic-javascript-with-react-node/', 'Isomorphic JavaScript');
+    var reactJSTrainingLink = this.createLink('https://reactjs-training.com/', 'React.js Training');
+    var ryanLink = this.createLink('https://twitter.com/ryanflorence', 'Ryan Florence');
+    var michaelLink = this.createLink('https://twitter.com/mjackson', 'Michael Jackson');
+    var workshopLink = this.createLink(Links.TICKET_SALES, 'grab a spot');
 
     return (
       <section id="about" style={STYLES.section}>
@@ -45,6 +49,11 @@ export default class About extends Component {
           Wether you're a seasoned developer, or a fledgling newbie.
           Whether you have been using React since it was first announced, or are just getting started.
           React Rally has something for everyone!
+        </p>
+        <p>
+          We have partnered with {reactJSTrainingLink} to offer a workshop following the conference.
+          If you want to take your React skills to the next level, come learn from {ryanLink} and {michaelLink}.
+          There are a limited number of seats for the workshop, and are not included with the conference ticket, so {workshopLink} while they last.
         </p>
       </section>
     );
