490f1f3e303bafbb959d205b81276cbe14699ece
Matt Zabriskie <mzabriskie@gmail.com>
2016-03-19 19:22:18 -0600

    Stubbing in content for Sponsors screen

--- 2016/app/screens/Sponsors/index.js
+++ 2016/app/screens/Sponsors/index.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default () => {
   return (
     <div className="Sponsors">
-      <h1>Sponsors</h1>
+      <h1>Thank you to our fine sponsors. Want to help support React Rally? We'd love to talk with you.</h1>
+      <button className="medium">Download Prospectus</button>
     </div>
   )
 }
