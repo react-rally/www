3b208a6e906b67b48a7b6fd79813b9b7ea16e547
Matt Zabriskie <mzabriskie@gmail.com>
2016-03-19 19:48:05 -0600

    Adding content for Venue screen

--- 2016/app/screens/Venue/index.js
+++ 2016/app/screens/Venue/index.js
@@ -3,7 +3,24 @@ import React from 'react'
 export default () => {
   return (
     <div className="Venue">
-      <h1>Venue</h1>
+      <h1>React Rally 2016 will be held in the beautiful Rose Wagner Performing Arts Center within walking distance of great food and hotels.</h1>
+      <div className="Venue__Wrapper">
+        <div className="Venue__Content">
+          <div className="col col-4">
+            <h2>Rose Wagner Performing Arts Center</h2>
+            <p>The Rose is a vibrant, active hub for emerging and established artists and arts organizations in Salt Lake City. This versatile venue houses three theaters as well as rehearsal and creation spaces for a host of performing arts companies.</p>
+            <button className="medium transparent">Get Directions &raquo;</button>
+          </div>
+          <div className="col col-4 align-right">
+            <img src="assets/img/RoseWagnerMap.png"/>
+          </div>
+        </div>
+      </div>
+      <div className="Venue__Hotel">
+        <h2>Hotel Monaco</h2>
+        <p>We have arranged a discount with Hotel Monaco for attendees of the conference. This is an amazing four star hotel, available for just $139 per night. There are a limited number of rooms available, so book your room early.</p>
+        <button className="medium">Book a Room &raquo;</button>
+      </div>
     </div>
   )
 }
