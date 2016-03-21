9ab4566b666c4dd18eeadd6fd869bee2ee251363
Matt Zabriskie <mzabriskie@gmail.com>
2016-03-20 19:55:55 -0600

    Updating About header

--- 2016/app/screens/About/index.js
+++ 2016/app/screens/About/index.js
@@ -5,7 +5,7 @@ import Legend from 'components/Legend'
 export default () => {
   return (
     <div className="About">
-      <h1>About</h1>
+      <h1>React Rally 2016</h1>
       <p>React Rally is a two day, single track conference for developers of all backgrounds using Facebook's ReactJS. Speakers will cover topics such as React Native, Flux, ES6, Isomorphic JavaScript, and so much more. Whether you're a seasoned developer, or a fledgling newbie. Whether you have been using React since it was first announced, or are just getting started. React Rally has something for everyone!</p>
       <div className="align-center">
         <Legend>Organizers</Legend>
