e2d70af91cba1ec56e1f84bb1790274c0960a157
Matt Zabriskie <mzabriskie@gmail.com>
2016-03-24 13:07:23 -0600

    Adding countdown for tickets

---
+++ 2016/app/components/Tickets.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import moment from 'moment'
+import constants from 'helpers/constants'
+import Button from 'components/Button'
+
+const TICKET_RELEASE = Date.parse('2016-03-25T12:00:00-06:00');
+
+function duration(timeInMillis) {
+  let intervals = {
+    'h': 3600000,
+    'm': 60000,
+    's': 1000,
+  }
+  let sb = ''
+
+  Object.keys(intervals).forEach((k) => {
+    let v = intervals[k]
+    let unit = Math.floor(timeInMillis / v)
+    timeInMillis %= v
+
+    if (sb.length > 0) sb += ':'
+    if (String(unit).length === 1) unit = '0' + unit
+    sb += unit + k
+  })
+  return sb
+}
+
+export default class Tickets extends Component {
+  render() {
+    let ticketsAvailable = Date.now() >= TICKET_RELEASE
+    let diff
+
+    if (!ticketsAvailable) {
+      diff = moment(TICKET_RELEASE).diff()
+      setTimeout(this.forceUpdate.bind(this), 1000)
+    }
+
+    return (
+      <div className="Tickets">
+      {ticketsAvailable ? (
+        <Button href={constants.Links.TICKET_SALES} className="large">Register Now</Button>
+      ) : (
+        <div className="Tickets__Countdown">
+          <small>Early Bird Tickets</small>
+          <h2 className="Tickets__Countdown__Timer">{duration(diff)}</h2>
+        </div>
+      )}
+      </div>
+    )
+  }
+}
