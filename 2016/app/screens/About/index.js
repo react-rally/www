4f4881a982104cb6fbc077f07015aea95be6676a
Matt Zabriskie <mzabriskie@gmail.com>
2016-05-19 22:45:35 -0600

    Fixing broken social links

--- 2016/app/screens/About/index.js
+++ 2016/app/screens/About/index.js
@@ -14,15 +14,15 @@ export default () => {
           name="Matt Zabriskie"
           title="Co-organizer"
           avatar="https://avatars2.githubusercontent.com/u/199035"
-          twitter="https://twitter.com/mzabriskie"
-          github="https://github.com/mzabriskie"
+          twitter="mzabriskie"
+          github="mzabriskie"
         />
         <Person
           name="Jamison Dance"
           title="Co-organizer"
           avatar="https://avatars0.githubusercontent.com/u/72027"
-          twitter="https://twitter.com/jergason"
-          github="https://github.com/jergason"
+          twitter="jergason"
+          github="jergason"
         />
       </div>
     </div>
