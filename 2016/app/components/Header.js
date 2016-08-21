fc36bf1808c7595ae63eb1cc87c06df55b43d458
Matt Zabriskie <mzabriskie@gmail.com>
2016-08-21 16:43:51 -0600

    Adding DateUtils for handling time/date stuff

--- 2016/app/components/Header.js
+++ 2016/app/components/Header.js
@@ -1,13 +1,20 @@
 import React, { Component, PropTypes } from 'react'
+import cx from 'classnames'
 import { Link } from 'react-router'
+import moment from 'moment'
 import constants from 'helpers/constants'
+import { MountainTime } from 'helpers/DateUtils'
 import Button from 'components/Button'
 import Tickets from 'components/Tickets'
 import Newsletter from 'components/Newsletter'
 
+const CONF_DAY_ONE_DATE = new Date(Date.parse(constants.Dates.CONF_DAY_ONE))
+const CONF_DAY_TWO_DATE = new Date(Date.parse(constants.Dates.CONF_DAY_TWO))
+const CONF_DATES_DISPLAY = moment(CONF_DAY_ONE_DATE).format('MMMM D') + '-' + moment(CONF_DAY_TWO_DATE).format('D')
+
 export default class Header extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {isNavActive: false}
   }
 
@@ -17,14 +24,15 @@ export default class Header extends Component {
 
   render() {
     let isHomeScreen = this.context.router.isActive('/', true)
-    let navClassName = (this.state.isNavActive ? ' Header__Nav--active ' : '') + 'Header__Nav navigation';
+    let isCFPOpen = MountainTime.isNowBetweenTime(Date.parse(constants.Dates.CFP_OPEN), Date.parse(constants.Dates.CFP_CLOSE))
+
     return (
       <header className={"Header" + (isHomeScreen ? "" : " Header--padded")}>
       {isHomeScreen ? (
         <div className="Home__Header">
           <div className="Home__Header__Wrapper">
             <div className="Home__Header__Content">
-              <h1>August 25-26 in Salt Lake City, UT</h1>
+              <h1>{CONF_DATES_DISPLAY} in Salt Lake City, UT</h1>
               <p>
                 Facebook's React has taken client side development by storm.
                 From single-page apps, to server rendering, to native mobile, to apps on your TV, React is everywhere.
@@ -32,6 +40,12 @@ export default class Header extends Component {
               </p>
               <div className="Home__Header__Buttons">
                 <Tickets/>&nbsp;&nbsp;&nbsp;&nbsp;
+              {isCFPOpen && (
+                <span>
+                  <Button href={constants.Links.PROPOSAL_FORM} className="large transparent">Submit Proposal</Button>
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                </span>
+              )}
                 <Button href={constants.Links.HOTEL_RESERVATION} className="large transparent">Book Hotel</Button>
               </div>
               <ul className="Home__Header__Nav navigation">
@@ -57,7 +71,14 @@ export default class Header extends Component {
             </Link>
           </div>
           <div className="Header__Nav__Wrapper col col-7 align-right">
-            <ul className={navClassName} onClick={this.handleNavClick.bind(this)}>
+            <ul
+              className={cx(
+                'navigation',
+                'Header__Nav',
+                {'Header__Nav--active': this.state.isNavActive}
+              )}
+              onClick={this.handleNavClick.bind(this)}
+            >
               <li><Link activeClassName="active" to="/speakers">Speakers</Link></li>
               <li><Link activeClassName="active" to="/schedule">Schedule</Link></li>
               <li><Link activeClassName="active" to="/venue">Venue</Link></li>
--- 2016/app/components/Tickets.js
+++ 2016/app/components/Tickets.js
@@ -1,38 +1,17 @@
 import React, { Component } from 'react'
 import constants from 'helpers/constants'
+import DateUtils, { MountainTime } from 'helpers/DateUtils'
 import Button from 'components/Button'
 
-const TICKET_RELEASE = Date.parse('2016-04-15T12:00:00-06:00');
-
-function duration(timeInMillis) {
-  let intervals = {
-    'd': 86400000,
-    'h': 3600000,
-    'm': 60000,
-    's': 1000,
-  }
-  let sb = ''
-
-  Object.keys(intervals).forEach((k) => {
-    let v = intervals[k]
-    let unit = Math.floor(timeInMillis / v)
-    timeInMillis %= v
-
-    if (sb.length === 0 && unit === 0) return;
-    if (sb.length > 0) sb += ':'
-    if (String(unit).length === 1) unit = '0' + unit
-    sb += unit + k
-  })
-  return sb
-}
+const TICKET_RELEASE_TIME = Date.parse(constants.Dates.TICKET_RELEASE)
 
 export default class Tickets extends Component {
   render() {
-    let ticketsAvailable = Date.now() >= TICKET_RELEASE
+    let ticketsAvailable = MountainTime.isNowAfterTime(TICKET_RELEASE_TIME)
     let diff
 
     if (!ticketsAvailable) {
-      diff = TICKET_RELEASE - Date.now()
+      diff = TICKET_RELEASE_TIME - MountainTime.getTime()
       setTimeout(this.forceUpdate.bind(this), 1000)
     }
 
@@ -43,7 +22,7 @@ export default class Tickets extends Component {
       ) : (
         <div className="Tickets__Countdown">
           <small>Tickets on sale soon</small>
-          <h2 className="Tickets__Countdown__Timer">{duration(diff)}</h2>
+          <h2 className="Tickets__Countdown__Timer">{DateUtils.duration(diff)}</h2>
         </div>
       )}
       </div>
