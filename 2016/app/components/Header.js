b5e4023a933f806cc79cc869ed47b653a7a2907d
Matt Zabriskie <mzabriskie@gmail.com>
2016-08-21 21:08:35 -0600

    Fixing Timezone issues

--- 2016/app/components/Header.js
+++ 2016/app/components/Header.js
@@ -8,8 +8,8 @@ import Button from 'components/Button'
 import Tickets from 'components/Tickets'
 import Newsletter from 'components/Newsletter'
 
-const CONF_DAY_ONE_DATE = new Date(Date.parse(constants.Dates.CONF_DAY_ONE))
-const CONF_DAY_TWO_DATE = new Date(Date.parse(constants.Dates.CONF_DAY_TWO))
+const CONF_DAY_ONE_DATE = MountainTime.createDate(Date.parse(constants.Dates.CONF_DAY_ONE))
+const CONF_DAY_TWO_DATE = MountainTime.createDate(Date.parse(constants.Dates.CONF_DAY_TWO))
 const CONF_DATES_DISPLAY = moment(CONF_DAY_ONE_DATE).format('MMMM D') + '-' + moment(CONF_DAY_TWO_DATE).format('D')
 
 export default class Header extends Component {
