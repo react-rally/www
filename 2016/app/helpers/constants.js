1d85827cf25f80bb55af478ca2964a0c057e5dfd
Matt Zabriskie <mzabriskie@gmail.com>
2016-08-22 13:02:15 -0600

    Stubbing in livestream

--- 2016/app/helpers/constants.js
+++ 2016/app/helpers/constants.js
@@ -7,6 +7,7 @@ constants.Links = {
   PROPOSAL_FORM: 'http://speak.reactrally.com/events/react-rally-2016',
   VENUE_DIRECTIONS: 'https://www.google.com/maps/dir//138+W+300+S,+Salt+Lake+City,+UT+84101/@40.7628941,-111.8974779,17z/data=!4m13!1m4!3m3!1s0x8752f50523c19fa1:0x4e421c3894d36491!2s138+W+300+S,+Salt+Lake+City,+UT+84101!3b1!4m7!1m0!1m5!1m1!1s0x8752f50523c19fa1:0x4e421c3894d36491!2m2!1d-111.8952892!2d40.7628941',
   HOTEL_RESERVATION: 'http://bookings.ihotelier.com/bookings.jsp?groupID=1629467&hotelID=4650',
+  LIVE_STREAM: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
   SPONSOR_PROSPECTUS: 'docs/sponsor.pdf'
 }
 
--- 2016/app/screens/Home/index.js
+++ 2016/app/screens/Home/index.js
@@ -4,16 +4,38 @@ import About from 'components/About'
 import Button from 'components/Button'
 import Legend from 'components/Legend'
 import Person from 'components/Person'
+import constants from 'helpers/constants'
+import { MountainTime } from 'helpers/DateUtils'
 import SpeakerData from '../../../api/speakers'
 
 export default () => {
+  let isEventLive = MountainTime.isNowBetweenTime(
+                      Date.parse(constants.Dates.CONF_DAY_ONE),
+                      Date.parse(constants.Dates.CONF_DAY_TWO)
+                    )
+
   return (
     <div className="Home">
-      <div>
-        <h2>What is React Rally?</h2>
-        <About />
-        <Link to="/about" className="Button medium">More about React Rally &raquo;</Link>
-      </div>
+      {isEventLive ? (
+        <div className="align-center">
+          <h2>Live Stream</h2><br/>
+          <iframe
+            className="Home__LiveStream"
+            width="560"
+            height="315"
+            src={constants.Links.LIVE_STREAM}
+            frameborder="0"
+            allowfullscreen
+            style={{border: 0}}
+          ></iframe>
+        </div>
+      ) : (
+        <div>
+          <h2>What is React Rally?</h2>
+          <About />
+          <Link to="/about" className="Button medium">More about React Rally &raquo;</Link>
+        </div>
+      )}
       <div className="align-center">
         <Legend>Keynote Speakers</Legend>
         {Object.keys(SpeakerData).map(key => {
