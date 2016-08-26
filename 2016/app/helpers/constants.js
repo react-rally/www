fb8ecbe369863e48ce9c0b40fb861f11028627bb
Matt Zabriskie <mzabriskie@gmail.com>
2016-08-26 10:53:30 -0600

    Adding address for after party

--- 2016/app/helpers/constants.js
+++ 2016/app/helpers/constants.js
@@ -8,6 +8,7 @@ constants.Links = {
   VENUE_DIRECTIONS: 'https://www.google.com/maps/dir//138+West+300+South,+Salt+Lake+City,+UT+84101/@40.7631303,-111.8964432,18.29z/data=!4m8!4m7!1m0!1m5!1m1!1s0x8752f5052002298f:0xf13b0bec7c31c1ab!2m2!1d-111.8955862!2d40.7631063',
   HOTEL_RESERVATION: 'http://bookings.ihotelier.com/bookings.jsp?groupID=1629467&hotelID=4650',
   HOTEL_DIRECTIONS: 'https://www.google.com/maps/dir//500+South+Main+Street,+Salt+Lake+City,+UT+84101/@40.7573935,-111.8947835,17z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x8752f510c9a96fd3:0xa6803ff714f0482d!2m2!1d-111.8925948!2d40.7573935',
+  PARTY_DIRECTIONS: 'https://www.google.com/maps/place/239+Main+St,+Salt+Lake+City,+UT+84111/@40.7644077,-111.890665,18z/data=!4m5!3m4!1s0x8752f50fb887bf49:0x2a34ca1a9e86b09f!8m2!3d40.7644448!4d-111.8895774',
   LIVE_STREAM_PLACEHOLDER: 'https://youtu.be/EfBWZYPnBxE',
   LIVE_STREAM_DAY_ONE: 'https://youtu.be/Fk--XUEorvc',
   LIVE_STREAM_DAY_TWO: 'https://youtu.be/nI0cQ-2YR1I',
