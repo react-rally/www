c68da0383a1d7265dcd3b37297c7e1bdaaab49d8
Matt Zabriskie <mzabriskie@gmail.com>
2017-05-08 12:44:44 -0600

    Adding ticket cards to home screen

--- 2017/app/components/Button.js
+++ 2017/app/components/Button.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 export default (props) => {
   return (
-    <a href={props.href} target="_blank" className={"Button " + props.className}>
+    <a
+      href={props.href}
+      target="_blank"
+      className={'Button' + (props.className ? ' ' + props.className : '')}
+      disabled={props.disabled}
+    >
       {props.children}
     </a>
   )
--- 2017/app/screens/Home/index.js
+++ 2017/app/screens/Home/index.js
@@ -5,9 +5,40 @@ import constants from 'helpers/constants'
 import shuffle from 'helpers/shuffle'
 import About from 'components/About'
 import Button from 'components/Button'
+import Card from 'components/Card'
 import Legend from 'components/Legend'
 import Person from 'components/Person'
 import SpeakerData from '../../../api/speakers'
+
+const Tickets = [
+  {
+    name: 'Early Bird - Round One',
+    price: 300,
+    description: 'Early bird tickets for React Rally! 25% off because we love early bird ticket buyers.',
+    soldOut: true
+  },
+  {
+    name: 'Early Bird - Round Two',
+    price: 350,
+    description: 'One last chance to grab a React Rally ticket at a discounted price.',
+    soldOut: true
+  },
+  {
+    name: 'Standard',
+    price: 400,
+    description: 'The real deal. One ticket to React Rally.'
+  },
+  {
+    name: 'Significant Other Activity Ticket',
+    price: 20,
+    description: 'Activities will be held both August 24 and August 25 in the evening after the conference. Purchase this ticket and bring your significant other along for the fun.'
+  },
+  {
+    name: 'Charitable Donation',
+    price: '-',
+    description: 'All proceeds from this ticket will be donated to your choice of Black Girls Code, Humane Society of Utah, or EFF. This won\'t grant you entrance to the conference, but it will help you feel good.'
+  }
+]
 
 export default () => {
   return (
@@ -25,6 +56,23 @@ export default () => {
         })}
       </div>
     ) : null}
+      <div className="align-center">
+        <Legend>Tickets</Legend>
+      {Tickets.map((t) => {
+        const price = (isNaN(t.price) ? '' : '$') + t.price
+
+        return (
+          <Card key={t.name} className="TicketCard">
+            <h3>{t.name}</h3>
+            <p>{t.description}</p>
+            <h2>{price}</h2>
+            <Button href="https://ti.to/trace-events/react-rally-2017/" disabled={t.soldOut}>
+              {t.soldOut ? 'Sold Out' : 'Buy Now'}
+            </Button>
+          </Card>
+        )
+      })}
+      </div>
     </div>
   )
 }
