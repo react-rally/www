3a718525c2165bfa6c4e9da91909761619f73cf5
Matt Zabriskie <mzabriskie@gmail.com>
2017-05-09 15:42:00 -0600

    Restoring assets

--- 2017/webpack.config.js
+++ 2017/webpack.config.js
@@ -3,13 +3,6 @@ var plugins = [];
 
 if (process.env.NODE_ENV === 'production') {
   plugins.push(
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    })
-  )
-  plugins.push(
     new webpack.optimize.UglifyJsPlugin()
   );
 }
