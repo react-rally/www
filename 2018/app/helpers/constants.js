0cc8f822e6a84545caed37fcfcdf88835c8ae78b
Matt Zabriskie <mzabriskie@apple.com>
2018-08-16 08:14:38 -0600

    Fixing livestream links

--- 2018/app/helpers/constants.js
+++ 2018/app/helpers/constants.js
@@ -5,13 +5,13 @@ constants.Links = {
   CODE_OF_CONDUCT: 'http://confcodeofconduct.com/',
   TICKET_SALES: 'https://ti.to/trace-events/react-rally-2018',
   PROPOSAL_FORM: 'http://speak.reactrally.com/events/react-rally-2018',
-  VENUE_DIRECTIONS: 'https://goo.gl/maps/V1kpixDA8DF2',
   HOTEL_RESERVATION: 'https://www.starwoodmeeting.com/Book/reactrally2018',
+  VENUE_DIRECTIONS: 'https://goo.gl/maps/V1kpixDA8DF2',
   PARTY_DIRECTIONS: 'https://goo.gl/maps/AeYKJrkAFZG2',
-  LIVE_STREAM_PLACEHOLDER: 'https://youtu.be/5NNOrp_83RU',
-  LIVE_STREAM_DAY_ONE: 'https://www.youtube.com/watch?v=nUhN03Uo6go',
-  LIVE_STREAM_DAY_TWO: 'https://www.youtube.com/watch?v=ii-T6HrkZFM',
-  LIVE_STREAM_OVER: 'https://youtu.be/T1XgFsitnQw',
+  LIVE_STREAM_PLACEHOLDER: 'https://www.youtube.com/embed/5NNOrp_83RU',
+  LIVE_STREAM_DAY_ONE: 'https://www.youtube.com/embed/nUhN03Uo6go',
+  LIVE_STREAM_DAY_TWO: 'https://www.youtube.com/embed/ii-T6HrkZFM',
+  LIVE_STREAM_OVER: 'https://www.youtube.com/embed/T1XgFsitnQw',
   YOUTUBE_CHANNEL: 'https://www.youtube.com/playlist?list=PLUD4kD-wL_zYSfU3tIYsb4WqfFQzO_EjQ',
   SPONSOR_PROSPECTUS: 'docs/sponsor.pdf'
 }
